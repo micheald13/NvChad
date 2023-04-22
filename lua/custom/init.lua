f703b8dde1a29d477fedc67548a6ba100bab0852
micheal <micheal.donohue@blackbox.com>
2023-04-22 10:43:19 +0100

    add highlight on yank

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,1 +1,9 @@
 vim.opt.colorcolumn = "80"
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", {}),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
