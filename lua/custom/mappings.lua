e59a92bae40d15fa45664d521848ff198fb95f3e
micheal <micheal.donohue@blackbox.com>
2023-04-22 10:43:38 +0100

    add some better telescope mappings

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,7 +3,10 @@ local M = {}
 M.abc = {
   n = {
     ["<leader>gg"] = {"<cmd>:LazyGit<CR>", "lazygit"},
-  }
+    ["<leader>/"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "find in current buffer" },
+    ["<leader>fw"] = { "<cmd> Telescope grep_string <CR>", "search current word" },
+    ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "live grep" },
+  },
 }
 
 return M
