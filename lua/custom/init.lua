33f5ed171bd2fe72c43c78bc06a32f85277609ad
micheal <micheal.donohue@blackbox.com>
2023-05-26 11:13:16 +0100

    Add spell check

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -7,3 +7,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+-- Enable spell checking for certain file types
+vim.api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
+  {
+    pattern = { "*.txt", "*.md", "*.tex" },
+    callback = function()
+      vim.opt.spell = true
+      vim.opt.spelllang = "en_gb"
+    end,
+  }
+)
