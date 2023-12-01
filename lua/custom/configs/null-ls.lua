9a88acf62eaeeff138d55dcba402f0de1815f5a1
micheal <micheal.donohue@blackbox.com>
2023-12-01 17:36:50 +0000

    Adding various changes

---
+++ lua/custom/configs/null-ls.lua
@@ -0,0 +1,58 @@
+local null_ls = require("null-ls")
+local helpers = require("null-ls.helpers")
+
+local formatting = null_ls.builtins.formatting
+local lint = null_ls.builtins.diagnostics
+
+local sources = {
+   formatting.prettier,
+   formatting.stylua,
+
+   lint.shellcheck,
+   lint.gccdiag,
+   -- lint.cppcheck,
+}
+
+null_ls.setup {
+   debug = true,
+   sources = sources,
+}
+
+-- local blackbox_build = {
+--     method = null_ls.methods.DIAGNOSTICS,
+--     filetypes = { "c", "cpp" },
+--     -- null_ls.generator creates an async source
+--     -- that spawns the command with the given arguments and options
+--     generator = null_ls.generator({
+--         command = "/home/developer/Emerald_sources/branches/develop_buildroot_update/BB_BUILD.sh",
+--         args = { "$PWD", "--encoder", "--zerou" },
+--         to_stdin = true,
+--         from_stderr = true,
+--         -- choose an output format (raw, json, or line)
+--         format = "line",
+--         check_exit_code = function(code, stderr)
+--             local success = code <= 1
+--
+--             if not success then
+--                 -- can be noisy for things that run often (e.g. diagnostics), but can
+--                 -- be useful for things that run on demand (e.g. formatting)
+--                 print(stderr)
+--             end
+--             print(stdout)
+--             return success
+--         end,
+--         -- use helpers to parse the output from string matchers,
+--         -- or parse it manually with a function
+--         on_output = helpers.diagnostics.from_patterns({
+--             {
+--                 pattern = [[:(%d+):(%d+) [%w-/]+ (.*)]],
+--                 groups = { "row", "col", "message" },
+--             },
+--             {
+--                 pattern = [[:(%d+) [%w-/]+ (.*)]],
+--                 groups = { "row", "message" },
+--             },
+--         }),
+--     }),
+-- }
+-- null_ls.register(blackbox_build)
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -21,5 +21,34 @@ local plugins = {
     'kdheepak/lazygit.nvim',
     lazy = false,
   },
+  {
+    'dhruvasagar/vim-table-mode',
+    lazy = false,
+  },
+  {
+  "neovim/nvim-lspconfig",
+
+   dependencies = {
+     "jose-elias-alvarez/null-ls.nvim",
+     config = function()
+       require "custom.configs.null-ls"
+     end,
+   },
+ 
+   config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+   end,
+  },
+  {
+    'dhruvasagar/vim-table-mode',
+    lazy = false,
+  },
+  {
+    'stevearc/oil.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  }
 }
 return plugins
